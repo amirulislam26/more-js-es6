e22c187277a19b6b102c60390806b13e15ac6284
amirulislam26 <26techamirulislam@gmail.com>
2022-11-11 06:50:32 -0800

    added a new more js es6 solving

---
+++ chainging.js
@@ -0,0 +1,16 @@
+const myObjects = {
+    x: 54,
+    y: 45,
+    z: 25,
+    a: 69,
+    b: 47
+};
+const {x, y, z, a, b} = myObjects;
+console.log(myObjects);
+
+//............array..................
+const [p, q] = [45, 47];
+console.log(p, q);
+
+const arrays = [45, 45, 47, 41, 25, 41, 26, 78];
+console.log(arrays);
