40d13bdfdd33069aad99638a64769af4bd51ea2b
amirulislam26 <26techamirulislam@gmail.com>
2022-11-11 06:59:21 -0800

    destructuring js added and solving

---
+++ destructuring.js
@@ -0,0 +1,11 @@
+const fish = {id: 58, name: 'king Hilsha', price: 9000,
+phone: '01302890996', address: 'rajbari', dress: 'silber'};
+// const phone = fish.phone;
+// const price = fish.price;
+// const dress = fish.dress;
+// const id = fish.id;
+// const address = fish.address;
+
+const update = {phone1, price, id, name} = fish;
+console.log(update);
+console.log(price);
